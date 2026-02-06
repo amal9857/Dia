60cea2fe53fd6f47af3296410c246e8105b7df85
amal9857 <Amal@admin.com>
2026-02-07 00:26:14 +0530

    Fix Flask app configuration

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,15 @@
-from flask import Flask, send_from_directory, send_file
+from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__)
-
-# Base directory
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/')
 def index():
-    return send_file(os.path.join(BASE_DIR, 'index.html'))
+    return send_from_directory('.', 'index.html')
 
-@app.route('/<path:filename>')
-def serve_file(filename):
-    return send_from_directory(BASE_DIR, filename)
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('.', path)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
