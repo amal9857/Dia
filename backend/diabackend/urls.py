30dc2f501788fe0995a342694fbe313daa7c71a6
amal9857 <Amal@admin.com>
2026-02-08 23:28:47 +0530

    Add frontend serving

--- backend/diabackend/urls.py
+++ backend/diabackend/urls.py
@@ -18,13 +18,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.http import FileResponse
 from rest_framework.routers import DefaultRouter
 from videos.views import VideoViewSet
+import os
 
 router = DefaultRouter()
 router.register(r'videos', VideoViewSet)
 
+def serve_frontend(request, path='index.html'):
+    frontend_dir = os.path.join(settings.BASE_DIR.parent)
+    file_path = os.path.join(frontend_dir, path)
+    if os.path.exists(file_path):
+        return FileResponse(open(file_path, 'rb'))
+    return FileResponse(open(os.path.join(frontend_dir, 'index.html'), 'rb'))
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('', serve_frontend),
+    path('<path:path>', serve_frontend),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
